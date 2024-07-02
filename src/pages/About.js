50dcdf5f4ad924e79470be8592b85eda9e507506
Nichol Fernin <ferninnichole@gmail.com>
2024-07-02 20:36:26 +0800

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,18 +12,14 @@ const About = () => {
         <div className="text-content">
           <h1>About Me</h1>
           <p>
-            Nichole is my name, Abaja is my mother's surname, Fernin is my father's last name.
-            So, basically I am Nichole Abaja Fernin. My mother got pregnant 21 years ago and she
-            gave birth on August 03, 2001 and that's me.
+            My name is Nichole. Abaja is my mother's surname, and Fernin is my father's last name. So, basically, I am Nichole Abaja Fernin. My mother got pregnant 21 years ago, and she gave birth to me on August 3, 2001.
           </p>
           <p>
-            Now I am studying at Central Philippines State University taking up Bachelor of Science 
-            in Information Technology. I am a third year college section D. I took this course 
-            because I want to be a programmer and web developer someday. That's why I am expecting 
-            my teachers will teach me well.
+            Now, I am studying at Central Philippines State University, pursuing a Bachelor of Science in Information Technology. I am a fourth-year college student in section D. I chose this course because I want to be a programmer and web developer someday. That's why I expect my teachers to teach me well.
           </p>
           <h2>Personal Interest</h2>
           <p>Cooking</p>
+          <p>Reading</p>
           <h2>Languages</h2>
           <p>Hiligaynon</p>
           <p>Tagalog</p>
@@ -36,3 +32,4 @@ const About = () => {
 };
 
 export default About;
+
